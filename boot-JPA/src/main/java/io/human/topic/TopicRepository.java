a1d9193f0c470550006236f571a0b38d370aab58
Sumit <sumitchinchansure.251@gmail.com>
2018-08-12 17:06:36 +0900

    Changed package structure

--- boot-JPA/src/main/java/io/human/topic/TopicRepository.java
+++
@@ -1,9 +0,0 @@
-package io.human.topic;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface TopicRepository extends CrudRepository<Topic, String> {
-
-	//CrudRepository<Topic, String> is class developed by Spring team which contains 
-	// all the basic CRUD operations one will need.
-}
